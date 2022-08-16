e6cb859bf674d139272a2d725328a736b8d1ad7c
Hamdan Asim <hamdanasim@Hamdans-MacBook-Pro.local>
2022-08-16 15:57:02 +0500

    peer.py A command line program that implements a peer to peer
    WHAT: A program that allows you to connect to a peer and chat with it, added some working code
    
    WHY: Because it was our assignment to complete the task
    
    IMPACT: HIGH

--- peer.py
+++ peer.py
@@ -1,55 +1,63 @@
 import datetime
 import socket
-import threading 
+import threading
 import pickle
 
 AllSockets = []
 
-def sendingTwoWay(c):
+
+def sendingTwoWay(sockets):
     while True:
         message = input("Enter your message: ")
-        c.sendall(message.encode())
-    
-def recievingTwoWay(c):
+        for c in sockets:
+            c.sendall(message.encode())
+
+
+def recievingTwoWay(sockets):
     while True:
-        message = c.recv(1024).decode()
-        print(message)    
-        
-def recieve(c,addr,listOfAdresses):
-    port=c.recv(1024).decode()
+        for c in sockets:
+            message = c.recv(1024).decode()
+            print(message)
+
+
+def recieve(c, addr, listOfAdresses):
+    port = c.recv(1024).decode()
     if port not in listOfAdresses:
         listOfAdresses.append(port)
     print("List of addresses: ", listOfAdresses)
-    send2 = threading.Thread(target= sendingTwoWay, args = (c,))
+    AllSockets.append(c)
+    send2 = threading.Thread(target=sendingTwoWay, args=(AllSockets,))
     send2.start()
     while True:
         message = c.recv(1024).decode()
-        if message != '':
+        if message != "":
             print(message)
         else:
             print("Connection closed with: ", addr)
             c.close()
             break
 
+
 def send(connectingPort, myPort):
-    connectionSocket=socket.socket()
+    print("creating new socket")
+    connectionSocket = socket.socket()
     print("Connecting socket created")
     connectionSocket.connect(("", connectingPort))
     connectionSocket.sendall((str(myPort)).encode())
-    
+
     if connectionSocket not in AllSockets:
         AllSockets.append(connectionSocket)
-    
-    recieving2 = threading.Thread(target = recievingTwoWay, args = (connectionSocket,))
+
+    recieving2 = threading.Thread(target=recievingTwoWay, args=(AllSockets,))
     recieving2.start()
     while True:
-        print("Allo " , str(len(AllSockets)))
+        print("Allo ", str(len(AllSockets)))
         message = input("Enter your message: ")
         for s in AllSockets:
             s.sendall(message.encode())
 
 
-messages=[]
+messages = []
 
 s = socket.socket()
 print("Socket succesfully created")
@@ -60,25 +68,25 @@ s.bind(("", port))
 print(f"socket binded to port{port}")
 s.listen(5)
 print("Socket is listening")
-listOfThreads=[None]*5
-i=0
+listOfThreads = [None] * 5
+i = 0
 listOfAddresses = []
 listOfAddresses.append(str(port))
 
 
-connectingPort=input("Enter the port you want to connect With: ")
-if connectingPort:   
+connectingPort = input("Enter the port you want to connect With: ")
+if connectingPort:
     listOfAddresses.append(str(connectingPort))
     print(listOfAddresses)
-    connectionThread=threading.Thread(target=send, args=(int(connectingPort),int(port)))
+    connectionThread = threading.Thread(
+        target=send, args=(int(connectingPort), int(port))
+    )
     connectionThread.start()
-    
-
 
 
 while True:
     c, addr = s.accept()
     print("Got connection from", addr)
-    listOfThreads[i]=threading.Thread(target=recieve, args=(c,addr,listOfAddresses) )
+    listOfThreads[i] = threading.Thread(target=recieve, args=(c, addr, listOfAddresses))
     listOfThreads[i].start()
-    i+=1
+    i += 1
